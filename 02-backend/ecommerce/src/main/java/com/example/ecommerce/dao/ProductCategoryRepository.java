567d2377a9428b8c97f4529e60a2a45bef6fe1a5
JR <jose.alonso.programmer@gmail.com>
2022-11-04 19:28:59 +0100

    Add CrossOrigin support to Spring Boot app

--- 02-backend/ecommerce/src/main/java/com/example/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/ecommerce/src/main/java/com/example/ecommerce/dao/ProductCategoryRepository.java
@@ -2,10 +2,11 @@ package com.example.ecommerce.dao;
 
 import com.example.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Repository
-//@RepositoryRestSource(collectionResourceRel = "productCategory", path = "product_category")
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product_category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
 }
--- 02-backend/ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
+++ 02-backend/ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.example.ecommerce.dao;
 
 import com.example.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")   // Origin is protocol, hostname and port
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
