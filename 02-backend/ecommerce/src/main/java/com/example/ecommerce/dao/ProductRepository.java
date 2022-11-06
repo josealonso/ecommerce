8634dbf197ce6e051c285da68b441c794b7a0390
JR <jose.alonso.programmer@gmail.com>
2022-11-06 01:45:22 +0100

    Implemented findByCategoryId endpoint

--- 02-backend/ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
+++ 02-backend/ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,18 @@
 package com.example.ecommerce.dao;
 
 import com.example.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")   // Origin is protocol, hostname and port
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    // Spring will execute a query similar to this:
+    // SELECT * FROM product where category_id=?
+    // Spring Data REST automatically exposes endpoint
+    // http:localhost:8080/api/products/search/findByCategoryId?id=2
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
