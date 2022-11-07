53bb9c31b8889f9a778ec65aed785de4227f12e5
JR <jose.alonso.programmer@gmail.com>
2022-11-07 02:19:08 +0100

    Modify Spring Boot app - Expose entity ids.

--- 02-backend/ecommerce/src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/ecommerce/src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,28 @@ package com.example.ecommerce.config;
 
 import com.example.ecommerce.entity.Product;
 import com.example.ecommerce.entity.ProductCategory;
+import org.hibernate.type.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -23,9 +37,29 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         // disable above methods for ProductCategory
         config.getExposureConfiguration()
-                .forDomainType(ProductCategory  .class)
+                .forDomainType(ProductCategory.class)
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // call an internal helper method
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        // get a list of all entity classes from the entity manager
+        // TODO solve this compile error.
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getClass());  // Not sure
+        }
+
+        // expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
 
     }
 }
