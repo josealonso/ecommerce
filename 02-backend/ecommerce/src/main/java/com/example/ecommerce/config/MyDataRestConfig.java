80e540c82664c43c7802258939cd630660887422
JR <jose.alonso.programmer@gmail.com>
2022-11-07 20:38:48 +0100

    Fixed small errors.

--- 02-backend/ecommerce/src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/ecommerce/src/main/java/com/example/ecommerce/config/MyDataRestConfig.java
@@ -2,7 +2,6 @@ package com.example.ecommerce.config;
 
 import com.example.ecommerce.entity.Product;
 import com.example.ecommerce.entity.ProductCategory;
-import org.hibernate.type.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -11,8 +10,10 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 
 @Configuration
@@ -48,18 +49,16 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     private void exposeIds(RepositoryRestConfiguration config) {
 
         // get a list of all entity classes from the entity manager
-        // TODO solve this compile error.
         Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
 
         // create an array of the entity types
         List<Class> entityClasses = new ArrayList<>();
         for (EntityType tempEntityType : entities) {
-            entityClasses.add(tempEntityType.getClass());  // Not sure
+            entityClasses.add(tempEntityType.getJavaType());
         }
 
         // expose the entity ids for the array of entity/domain types
         Class[] domainTypes = entityClasses.toArray(new Class[0]);
         config.exposeIdsFor(domainTypes);
-
     }
 }
