c497f92d3c023d6ff34f88f6d076fbfa8a118700
JR <jose.alonso.programmer@gmail.com>
2022-11-11 00:39:06 +0100

    Created service and repository classes for checkout.

--- 02-backend/ecommerce/src/main/java/com/example/ecommerce/dto/PurchaseResponse.java
+++ 02-backend/ecommerce/src/main/java/com/example/ecommerce/dto/PurchaseResponse.java
@@ -1,9 +1,12 @@
 package com.example.ecommerce.dto;
 
 import lombok.Data;
+import lombok.NonNull;
 
+// Lombok @Data will generate constructor for *final* fields
 @Data
 public class PurchaseResponse {
 
-    private String orderTrackingNumber;
+    private final String orderTrackingNumber;
+    // private @NonNull String orderTrackingNumber; // equivalent
 }
