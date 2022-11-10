c358a2c0edee1ed79b965450a9b16ad28238efc0
JR <jose.alonso.programmer@gmail.com>
2022-11-10 23:31:06 +0100

    Created JPA entities for the orders.

---
+++ 02-backend/ecommerce/src/main/java/com/example/ecommerce/entity/Address.java
@@ -0,0 +1,38 @@
+package com.example.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
