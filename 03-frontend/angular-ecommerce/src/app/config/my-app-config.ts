560a5e177a247bb6beda9d9809403ae168e40956
JR <jose.alonso.programmer@gmail.com>
2022-11-12 14:18:27 +0100

    Set up configuration for OpenID Connect using Okta.

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa789v3j1l7daVEk5d7',
+        issuer: 'https://dev-37958574.okta.com/oauth2/default',
+        redirectUrl: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
