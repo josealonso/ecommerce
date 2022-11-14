df7031f6f01ebc3cf54577fa8962cf18e6d85bb4
JR <jose.alonso.programmer@gmail.com>
2022-11-14 01:34:13 +0100

    Secure the orders endpoint on the backend.

---
+++ 02-backend/ecommerce/src/main/java/com/example/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(configurer ->
+                        configurer
+                                .antMatchers("/api/orders/**")
+                                .authenticated())
+                .oauth2ResourceServer()
+                .jwt();  // Enables JWT-encoded bearer token support
+
+        // add CORS filters
+        http.cors();
+
+        // add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class,
+                new HeaderContentNegotiationStrategy());
+
+        // force a non-empty response body for 401
+        Okta.configureResourceServer401ResponseBody(http);
+         
+        return http.build();
+    }
+}
