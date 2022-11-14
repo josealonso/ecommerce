9e6ba412e2a2aa8ee90f46f364d58a951bc626cf
JR <jose.alonso.programmer@gmail.com>
2022-11-14 16:52:43 +0100

    Changed Okta settings to use httpS.

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa78cq0ihnlx4giR5d7',
         issuer: 'https://dev-10137729.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
