417e6e3f0e52b6d22cb6fe0ec9ff541268ff63f1
JR <jose.alonso.programmer@gmail.com>
2022-11-15 20:16:28 +0100

    Added customer details field to the Stripe payment data.

--- 02-backend/ecommerce/src/main/java/com/example/ecommerce/service/CheckoutServiceImpl.java
+++ 02-backend/ecommerce/src/main/java/com/example/ecommerce/service/CheckoutServiceImpl.java
@@ -72,6 +72,7 @@ public class CheckoutServiceImpl implements CheckoutService {
         params.put("amount", paymentInfo.getAmount());
         params.put("currency", paymentInfo.getCurrency());
         params.put("payment_method_types", paymentMethodTypes);
+        params.put("description", "My ecommerce");
 
         return PaymentIntent.create(params);
     }
--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -246,7 +246,18 @@ export class CheckoutComponent implements OnInit {
           this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
             {
               payment_method: {
-                card: this.cardElement
+                card: this.cardElement,
+                billing_details: {
+                  email: purchase.customer.email,
+                  name: `${purchase.customer.firstName} ${purchase.customer.lastName}`,
+                  address: {
+                    line1: purchase.billingAddress.street,
+                    city: purchase.billingAddress.city,
+                    state: purchase.billingAddress.state,
+                    postal_code: purchase.billingAddress.zipCode,
+                    country: purchase.billingAddress.country
+                  }
+                }
               }
             }, { handleActions: false })
             .then((result: any) => {
@@ -281,6 +292,7 @@ export class CheckoutComponent implements OnInit {
     this.cartService.cartItems = [];
     this.cartService.totalPrice.next(0);  // sends 0 to all subscribers
     this.cartService.totalQuantity.next(0);
+    this.cartService.persistCartItems();
 
     // reset the form
     this.checkoutFormGroup.reset();
